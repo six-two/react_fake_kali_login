e35d2b5c716f6bb0e0af89bc9c065e7562c1b04d
six-two <info@six-two.dev>
2020-06-22 13:01:09 +1200

    Fixed: wrong github pages url. Removed old button

--- src/app/ScreenLogin.tsx
+++ src/app/ScreenLogin.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import StatusBar from './StatusBar';
-import { setScreen } from './redux/actions';
-import * as C from './redux/constants';
 import imageLoginBackground from '../img/background.jpg';
 
 
@@ -11,12 +9,7 @@ export default class ScreenLogin extends React.Component {
       <img className="fill-screen" src={imageLoginBackground} alt=""></img>
       <div className="fill-screen">
         <StatusBar />
-        <button onClick={this.screenOff}>Test: screen off</button>
       </div>
     </div>
-  }
-
-  screenOff = () => {
-    setScreen(C.SCREEN_SUSPEND);
   }
 }
