7c5644009cf05ed5e0c54d8a8ac882a9ccc044bf
six-two <info@six-two.dev>
2020-09-12 15:56:18 +1200

    Removed unneeded semicolons. Added a few blank lines

--- credential_server/example_server.py
+++ credential_server/example_server.py
@@ -12,10 +12,12 @@ PASSWORD_PARAM = 'p'
 DISK_PATH = "/disk.json"
 LOGIN_PATH = "/login.json"
 
+
 def isValidDiskPassword(password):
     if not password:
         return False
     return password == 'ok'
+
 
 def isValidLogin(username, password):
     print('Attempted login as "{}" with password "{}"'.format(username, password))
@@ -27,9 +29,10 @@ def isValidLogin(username, password):
         return True
     return False
 
+
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
-        isValid = DEFAULT_IS_VALID;
+        isValid = DEFAULT_IS_VALID
         print("\nGot request:", self.path)
         try:
             parsed = urlparse(self.path)
@@ -42,13 +45,13 @@ class Server(BaseHTTPRequestHandler):
                     return ''
 
             if parsed.path.endswith(DISK_PATH):
-                isValid = isValidDiskPassword(getArg(PASSWORD_PARAM));
+                isValid = isValidDiskPassword(getArg(PASSWORD_PARAM))
             elif parsed.path.endswith(LOGIN_PATH):
                 isValid = isValidLogin(getArg(USERNAME_PARAM), getArg(PASSWORD_PARAM))
             else:
                 print("Error: Unknown request type")
         except:
-            traceback.print_exc();
+            traceback.print_exc()
 
         print("Sending response: isValid={}".format(isValid))
         self.send_response(200)
@@ -59,7 +62,7 @@ class Server(BaseHTTPRequestHandler):
 
 
 def run(server_class=HTTPServer, host='localhost', port=3333):
-    host = 'localhost';
+    host = 'localhost'
     httpd = server_class((host, port), Server)
 
     print('Starting httpd on port %d...' % port)
@@ -75,6 +78,7 @@ def run(server_class=HTTPServer, host='localhost', port=3333):
         httpd.serve_forever()
     except KeyboardInterrupt:
         print("Ctrl-C: Shutting down...")
+
 
 if __name__ == "__main__":
     from sys import argv
